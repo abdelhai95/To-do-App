52eac4b719d75d5349961917c69b26a0dcf98914
abdelhai95 <abdelhaijamal95@gamil.com>
2023-01-19 20:42:49 +0200

    first version

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import { useState, useReducer, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import * as moment from "moment";
+import List from "./List.jsx";
+
+function reducer(state, action) {
+  // console.log(state);
+  switch (action.type) {
+    case "ADD-TASK":
+      return [...state, newItem(action.payload.item)];
+    case "EDIT":
+      return state.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, editing: !todo.editing, task: action.payload.text };
+        }
+        return todo;
+      });
+    case "DELETE":
+      return state.filter((todo) => todo.id !== action.payload.id);
+    default:
+      return state;
+  }
+}
+
+// Adding a new task with ID
+const newItem = function (item) {
+  return { id: Date.now(), task: item, editing: false };
+};
+
+function App() {
+  let date;
+  const [state, dispatch] = useReducer(reducer, []);
+  const [item, setItem] = useState("Add a new task");
+  function updateTime() {
+    date = moment().calendar();
+  }
+
+  updateTime();
+  setInterval(() => {
+    updateTime();
+  }, 1000);
+
+  const handleSubmit = function (e) {
+    e.preventDefault();
+    dispatch({ type: "ADD-TASK", payload: { item: item } });
+    // clear the input field after triggering the action
+    setItem("Add a new task");
+  };
+  return (
+    <section className="glass section">
+      <div className="date">
+        <p>{date}</p>
+      </div>
+      <div className="main-input-parent">
+        <input
+          className="main-input"
+          type="text"
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+          onFocus={(e) => setItem("")}
+        />
+        <button className="main-btn btn" type="submit" onClick={handleSubmit}>
+          Add Task
+        </button>
+      </div>
+      {state.map((item) => {
+        return <List key={item.id} item={item} dispatch={dispatch} />;
+      })}
+    </section>
+  );
+}
+
+export default App;
